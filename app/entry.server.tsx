0dbe39a246fdbf3371abd5f68bebea6b76889c9c
khamis1992 <khamis-1992@hotmail.com>
2025-09-28 00:26:29 +0300

    666

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,4 +1,4 @@
-import type { AppLoadContext } from '@remix-run/node';
+import type { AppLoadContext } from '@remix-run/cloudflare';
 import { RemixServer } from '@remix-run/react';
 import { isbot } from 'isbot';
 import { renderToReadableStream } from 'react-dom/server';
